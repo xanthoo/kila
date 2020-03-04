bd5be2e8ce80f8204e48c98db2db9736efb605da
i430228 <i430228@fhict.nl>
2020-03-04 04:38:58 -0800

    API class

---
+++ kilr-application/kilr-application/API.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace kilr_application
+{
+    class API
+    {
+        public API() { }
+
+        public void Get(String endpoint)
+        {
+            var json = new WebClient().DownloadString("http://i430228.hera.fhict.nl/api/" + endpoint + ".php");
+
+            Console.WriteLine(json);
+        }
+    }
+}
--- kilr-application/kilr-application/Form1.cs
+++ kilr-application/kilr-application/Form1.cs
@@ -8,14 +8,16 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace application
+namespace kilr_application
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
-        }
 
+            API api = new API();
+            api.Get("employees");
+        }
     }
 }
