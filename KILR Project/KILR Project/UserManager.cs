dac87da6501bf7d19d94c69aab35d5eda73cf142
i430228 <i430228@fhict.nl>
2020-03-11 15:49:49 -0700

    added User and UserManager classes

---
+++ KILR Project/KILR Project/UserManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KILR_Project
+{
+    class UserManager
+    {
+        private List<User> users;
+
+        public UserManager()
+        {
+            users = new List<User>();
+        }
+
+        public User GetUser(string username, string password)
+        {
+            return users.Find(user =>
+                user.Role != RoleEnum.Employee &&
+                user.Username == username &&
+                user.Password == password
+            );
+        }
+    }
+}
