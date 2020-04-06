e19f774c33f77004f64cfdbf92e5843c740d9805
Lukas <theluksys3@gmail.com>
2020-04-06 10:22:46 +0200

    base website

---
+++ website/core/config.example.php
@@ -0,0 +1,7 @@
+<?php
+
+// Database connection
+define('HOST',   '');
+define('DBNAME', '');
+define('USER',   '');
+define('PASS',   '');
---
+++ website/core/core.php
@@ -0,0 +1,8 @@
+<?php
+require_once "config.php";
+
+try {
+    $db = new PDO('mysql:dbname=' . DBNAME . ';host=' . HOST, USER, PASS);
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+}
