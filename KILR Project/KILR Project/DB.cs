aacec000390ff1dde51cd0a2d2ef5dc5607eddc1
i430228 <i430228@fhict.nl>
2020-04-14 22:57:21 -0700

    added base DB class and moved usermanager methods

---
+++ KILR Project/KILR Project/DB.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Windows.Forms;
+
+namespace KILR_Project
+{
+    public static class DB
+    {
+        private static string connectionString = "datasource=127.0.0.1;port=3306;username=root;password=;database=kilrdb;";
+
+        public static DataRow FindUser(string username, string password)
+        {
+            try
+            {
+                MySqlConnection connection = new MySqlConnection(connectionString);
+                DataTable dt = new DataTable();
+                MySqlDataAdapter adapter = new MySqlDataAdapter(
+                    "SELECT id, username, position FROM users WHERE username = @username AND password = @password",
+                    connection
+                );
+                adapter.SelectCommand.Parameters.AddWithValue("@username", username);
+                adapter.SelectCommand.Parameters.AddWithValue("@password", password);
+                connection.Open();
+                adapter.Fill(dt);
+
+                if (dt.Rows.Count == 0)
+                {
+                    return null;
+                }
+
+                return dt.Rows[0];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return null;
+            }
+        }
+    }
+}
